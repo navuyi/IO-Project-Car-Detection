36fb4f996f14ca7bd6d6e78fcb6d0b64b3c818ea
Michał Ryba <michalryba1206@gmail.com>
2020-12-07 03:43:11 +0100

    asf

--- Controller.py
+++ Controller.py
@@ -5,6 +5,7 @@ from Tracker import detect_and_save
 from tkinter import messagebox
 from Settings import WIDTH, HEIGHT
 
+
 class Controller:
     def __init__(self):
         self.root = tk.Tk()
@@ -18,15 +19,14 @@ class Controller:
 
         w = WIDTH
         h = HEIGHT
-        
+
         # calculate position x, y
-        x = (ws/2) - (w/2)    
-        y = (hs/2) - (h/2)
+        x = (ws / 2) - (w / 2)
+        y = (hs / 2) - (h / 2)
         self.root.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
         # Start main loop of the gui
         self.root.mainloop()
-
 
     def startAlgorithm(self, event):
         # Get frame offset value (default is 1)
@@ -35,20 +35,19 @@ class Controller:
         outputDir = self.gui.getDirectoryPath()
 
         # Check if input and output paths are selected
-        if(inputPath and outputDir):
+        if inputPath and outputDir:
             # Check input file format
-            if(inputPath.endswith(('.mp4', '.avi', '.mov', '.wmv','.mpg', '.mpeg', '.flv'))):
+            if inputPath.endswith(('.mp4', '.avi', '.mov', '.wmv', '.mpg', '.mpeg', '.flv')):
                 # Hide start button AND SHOW PROGRESS BAR OR SOMETHING XD
                 self.gui.startButton.place_forget()
 
                 # Start detection
-                detect_and_save(inputPath,outputDir,frameOffset)
+                detect_and_save(inputPath, outputDir, frameOffset, True)
             else:
                 messagebox.showwarning("Uwaga", "Wybierz poprawny format pliku wejściowego")
-                
+
         else:
             messagebox.showwarning("Uwaga", "Wybierz plik wejściowy oraz katalog wyjściowy")
-        
 
 
 controller = Controller()
